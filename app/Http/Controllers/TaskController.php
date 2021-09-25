31edf6a8872f4d7b5ec8f43d5350ea3d040612f3
Derick Vigne <derickvigne@me.com>
2021-09-25 14:12:51 -0400

    Added TaskController Skeleton

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Task Controller Logic for API Requests
+ */
+class TaskController extends Controller
+{
+  public function index() {
+    return "index";
+  }
+
+  public function create() {
+    return "create";
+  }
+
+  public function show() {
+    return "show";
+  }
+
+  public function update() {
+    return "update";
+  }
+
+  public function destroy() {
+    return "destroy";
+  }
+
+}
