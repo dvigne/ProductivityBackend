dc4208dccfa3fc18564c4baae45e01a18e8a88a9
Derick Vigne <derickvigne@me.com>
2021-09-26 06:02:30 -0400

    Updated Default Seeder to Create Dummy Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Task;
+use App\Models\Team;
+use App\Models\User;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Task::factory(50)->create();
+        Team::factory()->has(User::factory()->count(1), 'members')
+                       ->has(Task::factory()
+                        ->has(Comment::factory()->count(10))
+                        ->count(50))
+                       ->create();
+        // Team::factory(1)->create()->each(function($team) {
+        //   $team->members()->create();
+        //   $team->tasks()
+        // });
+        // $user = Team::first()->members()->create();
+
     }
 }
