af06a2c9e66a54e45a915385ebbc768e8f8c072b
Derick Vigne <derickvigne@me.com>
2021-09-25 21:35:39 -0400

    Added Route Middleware Group -- Published Configuration Keys for Auth0

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'Auth0' => \App\Http\Middleware\Auth0_JWT::class
     ];
 }
